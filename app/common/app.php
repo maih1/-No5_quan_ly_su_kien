d50a0a1784e5c7bd9117fad439102a2bf3f28b9e
Nam59157 <nn59157@gmail.com>
2021-12-22 15:32:01 +0700

    update

--- app/common/app.php
+++ app/common/app.php
@@ -1,43 +1,48 @@
 <?php
 class App{
 
-    protected $controller="ScheduleController";
+    protected $controller="Home";
     protected $action="show";
     protected $params=[];
 
     function __construct(){
  
         $arr = $this->UrlProcess();
-        unset($arr[0]);
+        // unset($arr[0]);
 
         // print_r($arr);
 
         // Controller
-        if( file_exists("./app/controller/".$arr[1]."controller.php") ){
-            $this->controller = $arr[1] . 'controller';
-            unset($arr[1]);
+        if( file_exists("./app/controller/".$arr[0]."Controller.php") ){
+            $this->controller = $arr[0] . 'Controller';
+            unset($arr[0]);
         }
         require_once "./app/controller/". $this->controller .".php";
         $this->controller = new $this->controller;
 
         // Action
-        if(isset($arr[2])){
-            if( method_exists( $this->controller , $arr[2]) ){
-                $this->action = $arr[2];
+        if(isset($arr[1])){
+            if( method_exists( $this->controller , $arr[1]) ){
+                $this->action = $arr[1];
             }
-            unset($arr[2]);
+            unset($arr[1]);
         }
 
         // Params
         $this->params = $arr?array_values($arr):[];
         call_user_func_array([$this->controller, $this->action], $this->params );
-        // var_dump($params);
+
+        // echo "</br>" . $this->controller;
+        // echo "</br>" . $this->action;
+        // echo "</br>";
+        // print_r($this->params);
     }
 
     function UrlProcess(){
-        if( isset($_SERVER['REQUEST_URI']) ){
-            // return explode("/", filter_var(trim($_GET["url"], "/")));
-            return explode("/", filter_var(trim($_SERVER['REQUEST_URI'], "/")));
+        // if( isset($_SERVER['REQUEST_URI']) ){
+        if( isset($_GET["url"]) ){
+            return explode("/", filter_var(trim($_GET["url"], "/")));
+            // return explode("/", filter_var(trim($_SERVER['REQUEST_URI'], "/")));
         }
     }
 
