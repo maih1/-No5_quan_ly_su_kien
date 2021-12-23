60074e390b905a6a11313aa8f39bb8b097326b27
maih1 <hoangmai.tq2000@gmail.com>
2021-12-23 23:45:26 +0700

    update form

--- app/Bridge.php
+++ app/Bridge.php
@@ -1,10 +1,10 @@
 <?php
-// Process URL from browser
-require_once "./app/common/App.php";
+    // Process URL from browser
+    require_once "./app/common/App.php";
 
-// How controllers call Views & Models
-require_once "./app/common/Controller.php";
+    // How controllers call Views & Models
+    require_once "./app/common/Controller.php";
 
-// Connect Database
-require_once "./app/common/DB.php";
+    // Connect Database
+    require_once "./app/common/DB.php";
 ?>
