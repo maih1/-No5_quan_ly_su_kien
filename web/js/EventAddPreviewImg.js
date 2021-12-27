9685a7a017fef9636f446ba07ea3a0fe3fb623b5
maih1 <hoangmai.tq2000@gmail.com>
2021-12-27 23:05:37 +0700

    update form

--- web/js/EventAddPreviewImg.js
+++
@@ -1,9 +0,0 @@
-var loadFile = function(event) {
-    var output = document.getElementById('output');
-    // console.log(output)
-    output.src = URL.createObjectURL(event.target.files[0]);
-    console.log(output.src)
-    output.onload = function() {
-        URL.revokeObjectURL(output.src) // free memory
-    }
-};
