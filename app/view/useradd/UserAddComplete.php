9d6b414b406eff83aacda75bf8d394ac6159db95
minh18112000 <nguyenducminh_t63@hus.edu.vn>
2021-12-30 15:58:22 +0700

    update AddComplete file

---
+++ app/view/useradd/UserAddComplete.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Đăng ký người dùng</title>
+	<meta charset=UTF-8>
+    <link rel="stylesheet" href="../web/css/UserAdd.css">
+</head>
+<body>
+    <form class="user-complete" method="post" action="">   
+        <p>Bạn đã đăng ký thành công thành viên.</p>
+        <a id='user-complete' href="<?php echo getUrl().'Home/home';?>">Trở về trang chủ</a>
+  </form>
+</body>
+</html>
