0cba7a1c3d4e650e4dcddad9229cf013ce105549
nvloc28052000 <vanloc28052000@gmail.com>
2022-01-01 10:29:37 +0700

    sua file reset.php

--- app/controller/reset.php
+++ app/controller/reset.php
@@ -2,66 +2,71 @@
 
 <html>
 
-    <head>
-        <link href="style.css" rel="stylesheet">
-        <meta charset="utf-8">
-    </head>
+<head>
+    <!--<link href="../../web/style.css" rel="stylesheet">-->
+    <meta charset="utf-8">
+    <title>Reset</title>
+</head>
 
-    <body>
-        <div>
-            <form action="reset.php" method="POST">
-                <table>
-                    <?php
-//                    $host = "localhost";
-//                    $dbname = "giuaki";
-//                    $username = "root";
-//                    $password = "";
-//                    $conn = new PDO("mysql:host=$host; dbname=$dbname; charset=utf8", $username, $password);
+<body>
 
-                    include '../common/db.php';
+    <div>
+        <form action="reset.php" method="POST">
+            <table>
+                <?php
+                include '../common/db.php';
+                $query = 'SELECT users.name, users.user_id  FROM users INNER JOIN admins ON admins.login_id = users.user_id WHERE admins.login_id = users.user_id AND admins.reset_password_token <> ""';
+                $sql = $conn->prepare($query);
+                $sql->setFetchMode(PDO::FETCH_ASSOC);
+                $sql->execute();
+                $result = $sql->fetchAll();
 
-                    $query = 'SELECT users.name FROM users INNER JOIN admins ON admins.login_id = users.user_id WHERE admins.login_id = users.user_id AND admins.reset_password_token <> ""';
-                    $sql = $conn->prepare($query);
-                    $sql->setFetchMode(PDO::FETCH_ASSOC);
-                    $sql->execute();
-                    $result = $sql->fetchAll();
-
-                    echo
-                    '<tr>
+                echo
+                '<tr>
                         <td>NO</td>                   
                         <td>Tên người dùng</td>
                         <td>Mật khẩu mới</td> 
                         <td>Action</td>   
                     </tr>';
-                    $i = 1;
-                    foreach ($result as $row) {
+                $i = 1;
+                foreach ($result as $row) {
 
-                        echo
-                        '<tr>
+                    echo
+                    '<tr>
                             <td>' . $i . '</td>                   
                             <td>' . $row["name"] . '</td>
-                            <td><input type="pasaword" name="input_reset"></td> 
-                            <td><button type="submit">Reset</button></td>                     
+                            <td><input type="text" name="input_reset"></td> 
+                            <td><button type="submit" class="button" name="button_reset' . $i . '">Reset</button></td>                     
                         </tr>';
-                        $i++;
-                    }
+                    $i++;
+                }
 
-                    if (isset($_POST["submit"])) {
-
+                $k = 1;
+                foreach ($result as $row) {
+                    if (isset($_POST["button_reset.$k"])) {
                         try {
                             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                            $new_password = $_POST["input_reset"];
-                            $a = $_POST["user_request"];
-                            $sql_1 = "UPDATE admins SET admins.reset_password_token ='' AND admins.password=MD5('$new_password') WHERE admins.login_id='$a'";
+                            $new_password = $_POST["input_reset.$k"];
+                            $i = $row["user_id"];
+                            $sql_1 = "UPDATE admins SET admins.password='$new_password' WHERE admins.login_id= '$i';
+                                         UPDATE admins SET admins.reset_password_token = \'\' WHERE admins.login_id= '$i';";
                             $stmt_1 = $conn->prepare($sql_1);
                             $stmt_1->execute();
                         } catch (Exception $ex) {
-                            echo $query_1 . "<br>" . $ex->getMessage();
+                            echo $sql_1 . "<br>" . $ex->getMessage();
                         }
                     }
-                    ?>
-                </table>  
-            </form>          
-        </div>
-    </body>
+                    $k++;
+                }
+                ?>
+            </table>
+        </form>
+    </div>
+
+
+
+
+
+</body>
+
 </html>
