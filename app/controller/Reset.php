2f41e5b83eb321f7dfd74bfd4db15000ede1e974
nvloc28052000 <vanloc28052000@gmail.com>
2022-01-02 09:41:43 +0700

    eidt Reset.php

--- app/controller/Reset.php
+++ app/controller/Reset.php
@@ -10,13 +10,14 @@ foreach ($result as $row) {
             $i = $row["user_id"];
             $new_password = $_POST["input_reset$k"];
             $new_password = trim($new_password);
-            if (strlen($new_password) >= 6) {
+
+            if (strlen($new_password) < 6) {
+                echo '<script>alert("Mật khẩu phải tối thiểu 6 kí tự")</script>';
+            } else {
                 $sql_1 = "UPDATE admins SET admins.password = '$new_password' WHERE admins.login_id = '$i';
                                 UPDATE admins SET admins.reset_password_token = '' WHERE admins.login_id = '$i';";
                 $stmt_1 = $conn->prepare($sql_1);
                 $stmt_1->execute();
-            } else {
-                echo '<script>alert("Mật khẩu phải tối thiểu 6 kí tự")</script>';
             }
         } catch (Exception $ex) {
             echo $sql_1 . "<br>" . $ex->getMessage();
