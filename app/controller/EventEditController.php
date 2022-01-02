041439c078f908b31321d9f0ab198b3e1e90d740
meodaymo <khgcanmotai@gmail.com>
2022-01-02 18:08:34 +0700

    Hoan thien Input

--- app/controller/EventEditController.php
+++ app/controller/EventEditController.php
@@ -4,156 +4,76 @@ require_once "./app/common/ErrorValidate.php";
 require_once "./app/model/EventEditModel.php";
 
 
-function eventEditInput(){
-
-    // $id = 2;
-    // $_SESSION["event_id"] = $id;
-    // $event = getEventbyId($id);
-
-    // $event_name = $event['name'];
-    // $event_slogan = $event['slogan'];
-    // $event_leader = $event['leader'];
-    // $event_description = $event['description'];
-    // $cur_event_avatar = $event['avatar'];
+function eventEditInput($event_id){
     
-    // $event_avatar = '';
+    $event = getEventbyId($event_id);
+    $event_name = $event['name'];
+    $event_slogan = $event['slogan'];
+    $event_leader = $event['leader'];
+    $event_description = $event['description'];
+    $event_avatar = $event['avatar'];
 
-    // $_SESSION["cur_event_avatar"] = $cur_event_avatar;
+    if($_SERVER["REQUEST_METHOD"] == "POST"){  
 
-    // if( isset($_SESSION["event_name"]) ){
-    //     $event_name = $_SESSION["event_name"];
-    // }
+        $event_name= test_input($_POST["event_name"]);
+        $event_slogan= test_input($_POST["event_slogan"]);
+        $event_leader= test_input($_POST["event_leader"]);
+        $event_description= test_input($_POST["event_description"]);
 
-    // if( isset($_SESSION["event_slogan"]) ){
-    //     $event_name = $_SESSION["event_solagan"];
-    // }
-    // if( isset($_SESSION["leader"]) ){
-    //     $event_name = $_SESSION["event_leader"];
-    // }
+        print_r($_FILES);
 
-    // if( isset($_SESSION["event_avatar"]) ){
-    //     $event_name = $_SESSION["event_avatar"];
-    // }
+        if(isset($_FILES)) {
+            $file_name =$_FILES["upload-file"]["name"]; // avatar mới
+            $temp_name = $_FILES["upload-file"]["tmp_name"];// khi upload file len file luu lai vi tri tam thoi, khi đủ điều kiện sẽ chuyển file từ vị trí tạm thời vào target_dir
+            $new_avatar = 1; // True, có avatar mới
+        } else {
+            $event_avatar = $event['avatar'];
+            $new_avatar = 0; // False, không có avatar mới, dùng lại avatar cũ
+        }
 
-    // if( isset($_SESSION["event_desciption"]) ){
-    //     $event_name = $_SESSION["event_description"];
-    // }
-
-    // // trả về   ssion_name_error nếu tồn và không null, mặt khác trả về ''
-    // $event_name_error = $_SESSION["event_name_error"] ?? '';
-    // $event_slogan_error = $_SESSION["event_slogan_error"] ?? '';
-    // $event_leader_error = $_SESSION["event_leader_error"] ?? '';
-    // $event_avatar_error = $_SESSION["event_avatar_error"] ?? '';
-    // $event_description_error = $_SESSION["event_description_error"] ?? '';
-
-    // if(isset($_POST['submit'])){
-    //     // $filename = $_FILES["file"]['name'];
-    //     // $tempname = $_FILES['file']['tmp_name'];
-    //     // $targetDir = $_SERVER['DOCUMENT_ROOT'] . '/No5_quan_ly_su_kien/web/avatar/' . $event_id . '/';
-    //     // $targetFilePath = $targetDir . $filename;
-
+        $validate = [];
+        $validate['name'] = empty($event_name) ? 'Hãy nhập tên sự kiện' : '';
+        $validate['name_length'] = strlen($event_name) > 100 ? 'Không nhập quá 100 ký tự. ' : '';
+        $validate['slogan'] = empty($event_slogan) ? 'Hãy nhập slogan ' : '' ;
+        $validate['slogan_length'] = strlen($event_slogan) > 250 ? 'Không nhập quá 250 ký tự.' : '';
+        $validate['leader'] = empty($event_leader) ? 'Hãy nhập leader sự kiện.' : '';
+        $validate['leader_length'] = strlen($event_leader) > 250 ? 'Không nhập quá 250 ký tự.' : '';
+        $validate['description'] = empty($event_description) ? 'Hãy nhập mô tả sự kiện.' : '';
+        $validate['description_length'] = strlen($event_description) > 1000 ? 'Không nhập quá 1000 ký tự.' : '' ; 
         
-    // }
- 
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $id = $_SESSION['event_id'];
-
-        $_SESSION["event_name_error"]= '';
-        $_SESSION["event_slogan_error"]='';
-        $_SESSION["event_leader_error"]='';
-        $_SESSION["event_avatar_error"]= '';
-        $_SESSION["event_description_error"]='';
-
-        $_SESSION['event_name']= test_input($_POST["event_name"]);
-        $_SESSION['event_slogan']= test_input($_POST["event_slogan"]);
-        $_SESSION['event_leader']= test_input($_POST["event_leader"]);
-        $_SESSION['event_description']= test_input($_POST["event_description"]);
-
-        if($_FILES["event_avatar"]["name"]){
-            unset($_SESSION["had_avatar_tmp"]);
-            if(
-                isset($_SESSION["event_avatar"]) &&
-                $_FILES["event_avatar"]["name"] != $_SESSION["event_avatar"]
-            ) {
-                unlink('../web/avatar/event_tmp');
-            }
-            $_SESSION["event_avatar"]= $_FILES["event_avatar"]["name"];
-        }
-
-        if(!$_SESSION["event_name"]){
-            $_SESSION["event_name_error"]= "Hãy nhập tên sự kiện.";
-        } else {
-            if(mb_strlen($_SESSION["event_name"]) > 100) {
-                $_SESSION["event_name_error"] = "Không nhập quá 100 ký tự.";
+        $checkValidate = 1;
+        foreach ($validate as $key => $value) {
+            if($value != '') {
+                $checkValidate = 0;
+                break;
             }
         }
 
-        if(!$_SESSION["event_slogan"]){
-            $_SESSION["event_slogan_error"]= "Hãy nhập slogan sự kiện.";
-        } else {
-            if(mb_strlen($_SESSION["event_slogan"]) > 250) {
-                $_SESSION["event_slogan_error"] = "Không nhập quá 250 ký tự.";
-            }
-        }
-
-        if(!$_SESSION["event_leader"]){
-            $_SESSION["event_leader_error"]= "Hãy nhập leader sự kiện.";
-        } else {
-            if(mb_strlen($_SESSION["event_leader"]) > 250) {
-                $_SESSION["event_leader_error"] = "Không nhập quá 250 ký tự.";
-            }
-        }
-
-        if(!$_SESSION["event_description"]){
-            $_SESSION["event_description_error"]= "Hãy nhập mô tả sự kiện.";
-        } else {
-            if(mb_strlen($_SESSION["event_description"]) > 1000) {
-                $_SESSION["event_description_error"] = "Không nhập quá 1000 ký tự.";
-            }
-        }
-
-        if(!file_exists('../web/avatar/evemt_tmp')) {
+        if(!is_dir('../web/avatar/event_tmp')) {
             mkdir('../web/avatar/event_tmp', 0777, true);
         }
-        
-        if ( !isset($_SESSION['had_avatar_tmp'])) {
-            if(
-                isset($_SESSION["event_avatar"]) &&
-                getimagesize($_FILES["event_avatar"]["tmp_name"]) == false
-            ){
-                $_SESSION["event_avatar_error"] = "avatar phải là file ảnh";
-            }
-            if(isset($_SESSION["event_avatar"])){
-                $target_dir = "../web/avatar/event_tmp";
-                $target_file = $target_dir . basename($_FILES["event_avatar"]["name"]);
-                move_uploaded_file($_FILES["event_avatar"]["tmp_name"], $target_file);
-                $_SESSION['had_avatar_tmp'] = true;
-            }
+
+        if($new_avatar){
+            $target_dir = "../web/avatar/event_tmp/";
+            $target_file = $target_dir . $file_name;
+            move_uploaded_file($temp_name, $target_file);
         }
 
-        if(
-            !$_SESSION["event_name_error"] &&
-            !$_SESSION["event_slogan_error"] &&
-            !$_SESSION["event_leader_error"] &&
-            !$_SESSION["event_avatar_error"] &&
-            !$_SESSION["event_description_error"]
-        ){
-            header("Location : ./app/view/eventedit/EventEditConfirm.php") ;
-        } else {
-            header("Location: ./EventEditInput");
+        if($checkValidate){
+            eventEditConfirm($event_id);
         }
 
     }
     
-    // require_once "./app/view/eventedit/EventEditInput.php";
+    require_once "./app/view/eventedit/EventEditInput.php";
 }
 
-function eventEditConfirm(){
+function eventEditConfirm($event_id){
+    
     require_once "./app/view/eventedit/EventEditConfirm.php";
 }
 
-function eventEditComplete(){
+function eventEditComplete($event_id){
     require_once "./app/view/eventedit/EventEditComplete.php";
 }
 
--- app/view/eventedit/EventEditInput.php
+++ app/view/eventedit/EventEditInput.php
@@ -1,93 +1,58 @@
-<?php 
-    $id = 2;
-    $_SESSION["event_id"] = $id;
-    $event = getEventbyId($id);
-
-    $event_name = $event['name'];
-    $event_slogan = $event['slogan'];
-    $event_leader = $event['leader'];
-    $event_description = $event['description'];
-    $cur_event_avatar = $event['avatar'];
-    
-    $event_avatar = '';
-
-    $_SESSION["cur_event_avatar"] = $cur_event_avatar;
-
-    if( isset($_SESSION["event_name"]) ){
-        $event_name = $_SESSION["event_name"];
-    }
-
-    if( isset($_SESSION["event_slogan"]) ){
-        $event_name = $_SESSION["event_solagan"];
-    }
-    if( isset($_SESSION["leader"]) ){
-        $event_name = $_SESSION["event_leader"];
-    }
-
-    if( isset($_SESSION["event_avatar"]) ){
-        $event_name = $_SESSION["event_avatar"];
-    }
-
-    if( isset($_SESSION["event_desciption"]) ){
-        $event_name = $_SESSION["event_description"];
-    }
-
-    // trả về   ssion_name_error nếu tồn và không null, mặt khác trả về ''
-    $event_name_error = $_SESSION["event_name_error"] ?? '';
-    $event_slogan_error = $_SESSION["event_slogan_error"] ?? '';
-    $event_leader_error = $_SESSION["event_leader_error"] ?? '';
-    $event_avatar_error = $_SESSION["event_avatar_error"] ?? '';
-    $event_description_error = $_SESSION["event_description_error"] ?? '';
-    
-?>
 <html>
 
 <head>
 	<title>Sửa sự kiện</title>
 	<meta charset=UTF-8>
-    <link rel="stylesheet" href="../web/css/EventEdit.css">
-    <script type="text/javascript" async src="../web/js/EventEditScript.js"></script>
+    <link rel="stylesheet" href="../../web/css/EventEdit.css">
+    <script type="text/javascript" async src="../../web/js/EventEditScript.js"></script>
+    <script type="text/javascript" async src="../../web/js/EventAddRealFileBtn.js"></script>
     
 </head>
 <body>
-    <form method="post" action="EventEditInput" enctype="multipart/form-data" >   
-        <div class="error">
-            <?php echo $event_name_error; ?>
-        </div>              
+    <form method="post" <?php echo 'action="../eventEditInput/' . $event_id . '"' ?> enctype="multipart/form-data" >   
+        <?php if(isset($validate)) {
+            if(array_key_exists('name', $validate)){
+                echo "<div class='error'>". $validate['name'] ."</div>";
+            }
+        } ?>
         <label class="input-form" for="name">Tên sự kiện</label>
-        <input id="event_name" type="text" name="event_name" maxlength="100" value="<?php echo $event_name;?>"><br>        
+        <input id="name" type="text" name="event_name" maxlength="100" value="<?php echo $event_name;?>"><br>        
         
-        <div class="error">
-            <?php echo $event_slogan_error; ?>
-        </div> 
+        <?php if(isset($validate)) {
+            if(array_key_exists('slogan', $validate)){
+                echo "<div class='error'>". $validate['slogan'] ."</div>";
+            }
+        } ?>
         <label class="input-form" for="slogan">Slogan</label>
         <input id="slogan" type="text" name="event_slogan" maxlength="250" value="<?php echo $event_slogan;?>"><br>        
         
-        <div class="error">
-            <?php echo $event_leader_error; ?>
-        </div> 
+        <?php if(isset($validate)) {
+            if(array_key_exists('leader', $validate)){
+                echo "<div class='error'>". $validate['leader'] ."</div>";
+            }
+        } ?> 
         <label class="input-form" for="leader">Leader</label>
         <input id="leader" type="text" name="event_leader" maxlength="250" value="<?php echo $event_leader;?>"><br>        
            
-        <div class="error">
-            <?php echo $event_description_error; ?>
-        </div>  
+        <?php if(isset($validate)) {
+            if(array_key_exists('description', $validate)){
+                echo "<div class='error'>". $validate['description'] ."</div>";
+            }
+        } ?>
         <label class="input-form" for="description">Mô tả chi tiết</label>
-        <textarea id="description" name="event_escription" maxlength="1000"><?php echo $event_description;?> </textarea><br>        
+        <textarea id="description" name="event_description" maxlength="1000"><?php echo $event_description;?> </textarea><br>        
         
         
         <!-- avatar cu -->
         <div style = "display: flex">
             <label class="input-form" for="avatar">Avatar</label>
-            <img class='avatar' src="../web/avatar/event/<?php echo $id; ?>/<?php echo $cur_event_avatar; ?>"/> <br>
+            <img class='avatar' src="../../web/avatar/event/<?php echo $event_id; ?>/<?php echo $event_avatar; ?>"/> <br>
         </div>
 
-        <div class="error">
-            <?php echo $event_avatar_error; ?>
-        </div> 
+
         <label class="input-form" for="avatar"></label>
         <input type="file" id="real-file" name="upload-file" hidden="hidden" accept="image/*"/>
-        <input id="custom-text" type="text" name="event_avatar" readonly="readonly" value="<?php echo $event_avatar;?>">
+        <input id="custom-text" type="text" name="event_avatar" readonly="readonly" value="">
         <button type="button" id="custom-button">Browse</button><br>
 
         <div class="button-submit">
