bcca5c4785b5cd2e07dd0db122bcfeac4d766d9a
nvloc28052000 <vanloc28052000@gmail.com>
2022-01-02 09:35:36 +0700

    eidt Reset.php

--- app/controller/reset.php
+++ app/controller/reset.php
@@ -9,10 +9,15 @@ foreach ($result as $row) {
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $i = $row["user_id"];
             $new_password = $_POST["input_reset$k"];
-            $sql_1 = "UPDATE admins SET admins.password = '$new_password' WHERE admins.login_id = '$i';
+            $new_password = trim($new_password);
+            if (strlen($new_password) >= 6) {
+                $sql_1 = "UPDATE admins SET admins.password = '$new_password' WHERE admins.login_id = '$i';
                                 UPDATE admins SET admins.reset_password_token = '' WHERE admins.login_id = '$i';";
-            $stmt_1 = $conn->prepare($sql_1);
-            $stmt_1->execute();
+                $stmt_1 = $conn->prepare($sql_1);
+                $stmt_1->execute();
+            } else {
+                echo '<script>alert("Mật khẩu phải tối thiểu 6 kí tự")</script>';
+            }
         } catch (Exception $ex) {
             echo $sql_1 . "<br>" . $ex->getMessage();
         }
