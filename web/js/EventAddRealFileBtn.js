db8f2d1478af601528fb387bef908137703f3644
maih1 <hoangmai.tq2000@gmail.com>
2022-01-04 21:02:13 +0700

    fix EventAddRealFileBtn.js

--- web/js/EventAddRealFileBtn.js
+++ web/js/EventAddRealFileBtn.js
@@ -1,7 +1,6 @@
-var realFileBtn = document.getElementById("file");
-var customBtn = document.getElementById("uploadTrigger");
+var realFileBtn = document.getElementById("real-file");
+var customBtn = document.getElementById("custom-button");
 var customTxt = document.getElementById("custom-text");
-var preview = document.getElementById("avatar-preview");
 
 if(customBtn) {
     customBtn.addEventListener("click", function() {
@@ -12,17 +11,9 @@ if(customBtn) {
 if(realFileBtn) {
     realFileBtn.addEventListener("change", function() {
     if (realFileBtn.value) {
-<<<<<<< HEAD:web/js/addRealFileBtn_comment.js
-        customTxt.value = realFileBtn.files[0].name;
-        preview.src = URL.createObjectURL(realFileBtn.files[0]);
-    } else {
-        customTxt.innerHTML = "No file chosen, yet.";
-    }
-=======
         customTxt.value = realFileBtn.value.match(
             /[\/\\]([\w\d\s\.\-\(\)]+)$/
         )[1];
     } 
->>>>>>> test_2:web/js/EventAddRealFileBtn.js
     });
 }
