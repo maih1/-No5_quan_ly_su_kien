46b877a3322b2a887f4a1fe234c8a13894fdd150
Nam59157 <nn59157@gmail.com>
2022-01-04 15:15:32 +0700

    update file index.php

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
+if(isset($_GET['url'])){
+    $paths = explode("/", filter_var(trim($_GET["url"], "/")));
+    $controller = $paths[0];
+    $function = $paths[1]  ?? '';
+} else {
+    $controller = 'Login';
+    $function = 'welcom';
+}
+require_once "./app/controller/". $controller ."Controller.php";
 
-$paths = explode("/", filter_var(trim($_GET["url"], "/")));
-require_once "./app/controller/". $paths[0] ."Controller.php";
-$paths[1]($paths[2] ?? '', $paths[3] ?? '');
+$function($paths[2] ?? '', $paths[3] ?? '');
 ?>
