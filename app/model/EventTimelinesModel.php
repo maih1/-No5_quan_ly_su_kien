a73c761601924407c43254d4d56c42f2cce41b23
Nam59157 <nn59157@gmail.com>
2022-01-06 16:28:34 +0700

    editSchedule

--- app/model/EventTimelinesModel.php
+++ app/model/EventTimelinesModel.php
@@ -19,4 +19,33 @@
         $result->execute();
         return $result;
 	}
+
+    function editEventTimelines($event_id, $id, $from, $to, $name, $detail, $PoC) {
+        global $conn;
+        
+        //$detail = addslashes($detail);
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $date = date('Y-m-d H:i:s');
+        $sql = "UPDATE `event_timelines` SET
+            
+            `from` = '$from',
+            `to` = '$to',
+            `name` = '$name',
+            `detail` = '$detail',
+            `PoC` = '$PoC'
+        WHERE event_id = '$event_id' AND id = '$id' ";
+        $result = $conn -> prepare($sql);
+        $result -> execute();
+        return $result;
+    }
+
+    function getChosenEvent($event_id, $id) {
+        global $conn;
+        
+        $sql = "SELECT * FROM `event_timelines` WHERE event_id = '$event_id' and id = '$id'";
+        $result = $conn -> prepare($sql);
+        $result -> execute();
+        $data = $result -> fetch(PDO::FETCH_ASSOC);
+        return $data;
+    }
 ?>	
