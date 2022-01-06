c67972b69f15bbe442f77fb6abaddd0135288db7
milt112 <68313673+milt112@users.noreply.github.com>
2022-01-06 15:37:13 +0700

    user model

--- app/model/UserAddModel.php
+++ app/model/UserAddModel.php
@@ -48,4 +48,38 @@
         
         return $check_add;
     }
+
+    function userSearch($key) {
+        global $conn;
+        //$key = isset($_GET['key']) ? $_GET['key'] : '';
+        $key = "%".$_GET['key']."%";
+        $query = "SELECT * FROM users WHERE name LIKE :key OR description LIKE :key";
+        $_query = $conn->prepare($query);
+        $_query->bindParam(':key', $key);
+        $_query->execute();
+        $result = $_query->fetchAll();
+
+        return $result;
+    }
+
+    function userSearchExact($phanloai) {
+        global $conn, $classify;
+        $phanloai = $_GET['phanloai'];
+        $query = "SELECT * FROM users WHERE type = :classify ";
+        $_query = $conn->prepare($query);
+        $_query -> bindParam(":classify", $classify[$phanloai]);
+        $_query->execute();
+        $count = $_query->fetchAll();
+
+        return $count;
+    }
+    
+    function userDel($id) {
+        global $conn;
+        $sql = "DELETE FROM users WHERE id = :id";
+        $del = $conn->prepare($sql);
+        $del->bindParam(':id', $id);
+        $del->execute();
+    }
+    
 ?>
