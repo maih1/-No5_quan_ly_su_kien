f63b6d8a9d50491e45b2a3e15e9271b86315dfac
luung1704 <71346057+luung1704@users.noreply.github.com>
2022-01-06 16:22:58 +0700

    Add files via upload

--- app/view/eventsearch/EventSearch.php
+++ app/view/eventsearch/EventSearch.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <form method="GET" enctype="multipart/form-data" >   
+		<div ><button id="back" type="button" name="schedule" onclick="window.location.href='../Login/home/'" formaction="">Quay lại</button> </div>
         <label class="keyword" for="keyword">Từ khóa</label>
         <input id="keyword" type="text" name="keyword" maxlength="100" value="<?php echo $keyword;?>"> </input>        
         <div class="button-search">
@@ -14,7 +15,7 @@
         </div> 
 		<p > Số sự kiện tìm thấy:<?php if(isset($event_search_result)) echo ' ',sizeof($event_search_result); else echo ' ',0;?> </p>
 		<table class="search-table">
-		<tr ><th class="search-no-th">No</th><th class="search-th">Tên sự kiện</th><th class="search-th">Slogan</th><th class="search-th">Leader</th><th class="search-th">Action</th></tr>
+		<tr ><th class="search-no-th">No</th><th class="search-th">Tên sự kiện</th><th class="search-th">Slogan</th><th class="search-th">Leader</th><th class="action-th">Action</th></tr>
 		<?php
 		if (isset($event_search_result)) {
 			foreach ($event_search_result as $key => $value) {
@@ -24,7 +25,7 @@
 				<th class="search-th"><?php echo $value[1];?></th>
 				<th class="search-th"><?php echo $value[2];?></th>
 				<th class="search-th"><?php echo $value[3];?></th>
-				<th class=search-th> 
+				<th class="action-th"> 
 				<button class="delete-button" type="submit" name="delete" onclick="return confirm('Bạn chắc chắn muốn xóa sự kiện <?php echo $value[1];?>');"value ="<?php echo $value[0];?>">Xóa</button>
 				<button class="delete-button" type="button" name="edit" onclick="window.location.href='../EventEdit/eventEditInput/<?php echo $value[0];?>'" formaction="">Sửa</button>
 				<button class="action-button" type="button" name="schedule" onclick="window.location.href='../EventTimelines/addSchedule/<?php echo $value[0];?>/<?php echo $keyword;?>'" formaction="">Lịch trình</button>
