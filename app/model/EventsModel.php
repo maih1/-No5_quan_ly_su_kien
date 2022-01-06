0a872384936abc8fb53122ce47a66e86c767eef8
maih1 <hoangmai.tq2000@gmail.com>
2022-01-06 11:45:33 +0700

    update model and css event add

--- app/model/EventsModel.php
+++ app/model/EventsModel.php
@@ -1,7 +1,50 @@
-<?php
-    require_once "./app/common/db.php";
+<?php 	
+	require_once "./app/common/db.php";
 
-    function getAll(){
+	function getEventName($event_id) {
+		global $conn;
+		$sql = "SELECT * FROM `events` WHERE id = '$event_id'";
+        $result = $conn -> prepare($sql);
+        $result->execute();
+        $_result = $result->fetchAll();
+        return $_result;
+	}
+
+	/* Event Edit
+    * Kết nối db, trả về dữ liệu
+    */
+    function getEventbyId($id){
+        global $conn;
+
+        $sql = "SELECT *
+                FROM events
+                WHERE id = '$id'
+                ";
+        
+        $event = $conn -> prepare($sql);
+        $event->execute();
+        return $event->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /* Update dữ liệu lên db
+    * Event Edit
+    */
+    function updateEventById($id, $name, $slogan, $leader, $avatar, $description){
+        global $conn;
+        $sql= " UPDATE events SET
+                name = '$name',
+                slogan = '$slogan',
+                leader = '$leader',
+                avatar = '$avatar',
+                description = '$description'
+                WHERE id = '$id' ";
+        $event = $conn ->prepare($sql);
+        return $event->execute();
+
+        
+    }
+	/* Event Add */
+	function getAll(){
         global $conn;
 
         $stmt = $conn -> prepare("SELECT * FROM `events`");
@@ -10,17 +53,17 @@
         $result = $stmt->fetchAll();
         return $result;
     }
-
+	/* Event Add */
     function getIdEnd(){
         global $conn;
-        $stmt = $conn -> prepare("SELECT `id` FROM `events`;");
+        $stmt = $conn -> prepare("SELECT `id` FROM `events` ORDER BY `events`.`id` ASC");
         $stmt->execute();
         $data = $stmt->fetchAll(PDO::FETCH_COLUMN);
         $length = count($data) - 1;
         $result = $data[$length];
         return $result;
     }
-
+	/* Event Add */
     function add(){
         global $conn;
         $check_add = false;
