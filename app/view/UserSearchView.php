82d41bdac113e04d1ddb9d525b606501f0148337
maih1 <hoangmai.tq2000@gmail.com>
2022-01-06 13:05:27 +0700

    update user search

--- app/view/UserSearchView.php
+++ app/view/UserSearchView.php
@@ -6,7 +6,7 @@
     <meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User search</title>
-    <link rel="stylesheet" href="../web/css/user_search_view.css">
+    <link rel="stylesheet" href="../web/css/UserSearchView.css">
 
 </head>
 <body>
