0609c374c06d3b78d08032c2b49ecc924fac12e2
jake1512 <dokhoinguyen.nguyen@gmail.com>
2022-01-06 08:39:35 -0500

    more filename fix

--- app/view/EditComment.php
+++ app/view/EditComment.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../../../web/css/comments.css"/>
+    <link rel="stylesheet" type="text/css" href="../../../web/css/Comments.css"/>
     <script async src="../../../web/js/CommentAddRealFileBtn.js"></script>
     <title>Edit Event Comment</title>
 </head>
