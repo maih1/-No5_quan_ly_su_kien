29733d66efe25af58c85acf54a07921abdc307f9
longngyen <longdaica442@gmail.com>
2022-01-06 20:07:03 +0700

    first comit

--- app/view/home.php
+++ app/view/home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../web/css/home.css">
+    <link rel="stylesheet" type="text/css" href="../web/css/Home.css">
 </head>
 <body>
     Tên login: 
