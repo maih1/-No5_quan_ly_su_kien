35ee604c4fa2a41de86411511711f95cc6e34a01
luung1704 <71346057+luung1704@users.noreply.github.com>
2022-01-06 21:46:37 +0700

    Add files via upload

--- app/controller/EventSearchController.php
+++ app/controller/EventSearchController.php
@@ -2,13 +2,33 @@
     require_once './app/common/CheckLogin.php';
 	require_once "./app/common/ErrorValidate.php";
     require_once "./app/model/EventSearchModel.php";
-    function eventSearch() {
+    function delete_directory($dirname) {
+		if (is_dir($dirname))
+			$dir_handle = opendir($dirname);
+			if (!$dir_handle)
+				return false;
+		while($file = readdir($dir_handle)) {
+			if ($file != "." && $file != "..") {
+				if (!is_dir($dirname."/".$file))
+					unlink($dirname."/".$file);
+				else
+					delete_directory($dirname.'/'.$file);
+			}
+		}
+		closedir($dir_handle);
+		rmdir($dirname);
+		return true;
+	}
+	function eventSearch() {
+		//error_reporting(0);
         global $event_search_result,$keyword;
 		if (isset($_GET['keyword']))
 			$keyword=$_GET['keyword'];
 		if (isset($_GET['delete'])){ 
 			eventDeleteSQL($_GET['delete']);
+			$id=$_GET['delete'];
 			$event_search_result= getEventSearchResult($_GET['keyword']);
+			delete_directory('./web/avatar/event/'.$id.'');
 		}
 		if (isset($_GET['submit']) || isset($keyword)){
 			$event_search_result= getEventSearchResult($_GET['keyword']);
