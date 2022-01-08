3cbb18b163b486a8d4053c614afd9c28af1e0485
luung1704 <71346057+luung1704@users.noreply.github.com>
2022-01-08 20:48:19 +0700

    Update EventsModel.php

--- app/model/EventsModel.php
+++ app/model/EventsModel.php
@@ -92,8 +92,8 @@
         return $check_add;
     }
 	function getEventSearchResult($keyword){
-        global $conn;
-        $keyword = "%".$_GET['keyword']."%";
+        	global $conn;
+        	$keyword = "%".$keyword."%";
 		$stmt = $conn -> prepare("SELECT id,name,slogan,leader FROM events WHERE name LIKE :keyword OR description LIKE :keyword OR leader LIKE :keyword OR slogan LIKE :keyword");
 		$stmt->bindParam(':keyword', $keyword);
 		$stmt->execute();
@@ -101,7 +101,7 @@
         return $result;
     }
 	function eventDeleteSQL($id){
-        global $conn;
+        	global $conn;
 		$stmt = $conn -> prepare("DELETE FROM event_comments WHERE event_id=:id");
 		$stmt->bindParam(':id', $id);
 		$stmt->execute();
