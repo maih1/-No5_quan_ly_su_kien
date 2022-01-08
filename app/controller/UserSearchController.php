cc61e3ed81b4289b99b111223b99b66decfd3f37
milt112 <68313673+milt112@users.noreply.github.com>
2022-01-08 16:39:21 +0700

    search

--- app/controller/UserSearchController.php
+++ app/controller/UserSearchController.php
@@ -1,24 +1,20 @@
 <?php
-require_once './app/common/CheckLogin.php';
-require_once './app/model/UserModel.php';
-$classify = array(1 => "Sinh viên", 2 => "Giáo viên", 3 => "Sinh viên cũ");
-function userSearchF()
-{
-    global $search, $key, $phanloai;
-    global $classify;
-    if (isset($_GET['key']))
-        $key = $_GET['key'];
-    if (isset($_GET['phanloai']))
-        $phanloai = $_GET['phanloai'];
-    if (isset($_GET['del'])) {
-        userDel($_GET['del']);
-    }
-    if (isset($_GET['submit'])) {
-        if (!empty($key)) {
-            $search = userSearch($_GET['key']);
-        } else {
-            $search = userSearchExact($_GET['phanloai']);
-        }
-    }
-    require_once './app/view/UserSearchView.php';
+    require_once './app/common/CheckLogin.php';
+    require_once './app/model/UserModel.php';
+    $classify = array(1 => "Sinh viên", 2 => "Giáo viên", 3 => "Sinh viên cũ");
+    function userSearchF() {
+        global $search, $key, $phanloai;
+		global $classify;
+		if (isset($_GET['key']))
+			$key=$_GET['key'];
+        if (isset($_GET['phanloai']))
+            $phanloai=$_GET['phanloai'];
+		if (isset($_GET['del'])){ 
+			userDel($_GET['del']);
+		}
+		if (isset($_GET['submit'])){
+            $search = userSearch($_GET['key'], $_GET['phanloai']);
+
+		}
+        require_once './app/view/UserSearchView.php';
 }
